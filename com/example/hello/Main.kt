85528e49a097b1aa3a82b2a125c8c9686ecd81ca
monya-9 <lee99714@naver.com>
2023-01-09 11:13:58 +0900

    sum Expression

---
+++ com/example/hello/Main.kt
@@ -0,0 +1,14 @@
+package com.example.hello
+
+
+fun sum(a:Int, b:Int):Int{
+    return a + b
+}
+
+fun sumEx(a: Int, b: Int) = a+b
+
+fun main() {
+    println(sum(1, 2));
+    println(sumEx(2, 3));
+}
+
